3205130ae9d263269107e9c6562d29d03cab2031
pedromcsimoes <pedromcsimoes99@hotmail.com>
2017-06-04 19:31:51 +0100

    Jogo do galo v 1.0

--- PS1617/src/java/beans/Gestaojogos.java
+++ PS1617/src/java/beans/Gestaojogos.java
@@ -1,17 +1,27 @@
 package beans;
 
 import autenticacao.Util;
+import com.sun.faces.component.visit.FullVisitContext;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.visit.VisitCallback;
+import javax.faces.component.visit.VisitContext;
+import javax.faces.component.visit.VisitResult;
+import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
+import logica.Jogada;
 import logica.Jogo;
 import logica.Logica;
 import logica.Logica.TipoJogo;
 import logica.Sessao;
+import org.primefaces.component.commandbutton.CommandButton;
 import org.primefaces.context.RequestContext;
 
 @ManagedBean
@@ -37,12 +47,12 @@ public class Gestaojogos implements Serializable {
         if (username != null) {
             if(sessao.getJogoId() < 1){
                 sessao.setJogoId(lo.iniciarJogo(username, tipoJogo));
-                return "/area_privada/gestaojogos?faces-redirect=true";
+                return "/area_privada/gestaojogos";
             }
             else{
                 RequestContext r = RequestContext.getCurrentInstance();
                 r.execute("PF('dlgIni').show();");                
-                return "/area_privada/gestaojogos?faces-redirect=true";
+                return "/area_privada/gestaojogos";
             }
         }
         return "login";
@@ -52,9 +62,9 @@ public class Gestaojogos implements Serializable {
         if (username != null && !lo.getJogo(id).getCriador().equals(username)) {
             lo.juntarJogo(id, username);
             sessao.setJogoId(id);
-            return "/area_privada/jogo?faces-redirect=true";
+            return "/area_privada/jogo";
         }
-        return "/area_privada/gestaojogos?faces-redirect=true";
+        return "/area_privada/gestaojogos";
     }
     
     public boolean possoJuntar(int id){
@@ -77,7 +87,7 @@ public class Gestaojogos implements Serializable {
     
     public String jogar(int id){
         sessao.setJogoId(id);
-        return "/area_privada/jogo?faces-redirect=true";
+        return "/area_privada/jogo";
     }
     
     public ArrayList<Jogo> listarJogosIniciados() {
@@ -93,20 +103,20 @@ public class Gestaojogos implements Serializable {
             return verificaTerminaJogo();
         }if(lo.jogoTerminado(sessao.getJogoId())){
             sessao.setJogoId(-1);
-            return "/area_privada/gestaojogos?faces-redirect=true";
+            return "/area_privada/gestaojogos";
         }else{
             RequestContext r = RequestContext.getCurrentInstance();
             r.execute("PF('dlg').show();");
-            return "/area_privada/jogo?faces-redirect=true";
+            return "/area_privada/jogo";
         }
     }
 
     public String verificaTerminaJogo() {
         if(lo.terminaJogo(sessao.getJogoId())){
             sessao.setJogoId(-1);
-            return "/area_privada/gestaojogos?faces-redirect=true";
+            return "/area_privada/gestaojogos";
         }
-        return "/area_privada/jogo?faces-redirect=true";
+        return "/area_privada/jogo";
     }
     
     @PostConstruct
@@ -118,36 +128,110 @@ public class Gestaojogos implements Serializable {
 
 //----------------------------//--------------------------    
 
-    public String joga(int pos){
-        boolean ok;
-        boolean t=false;
-        switch (pos) {
-            case 0:
-                ok = lo.joga(username, pos);
-                break;
-            case 1:
-                ok = lo.termina(username, 1);
-                t=true;
-                break;
-            case 2:
-                ok = lo.termina(username, -1);
-                t=true;
-                break;
-            default:
-                ok = lo.termina(username, 0);
-                t=true;
-                break;
+    public UIComponent findComponent(final String id) {
+
+        FacesContext context = FacesContext.getCurrentInstance(); 
+        UIViewRoot root = context.getViewRoot();
+        final UIComponent[] found = new UIComponent[1];
+
+        root.visitTree(new FullVisitContext(context), new VisitCallback() {     
+            @Override
+            public VisitResult visit(VisitContext context, UIComponent component) {
+                if(component.getId().equals(id)){
+                    found[0] = component;
+                    return VisitResult.COMPLETE;
+                }
+                return VisitResult.ACCEPT;              
+            }
+        });
+
+        return found[0];
+    }
+    
+    public void joga(int pos){
+        boolean ok;        
+        String id = "btn" + pos;
+        ok = lo.joga(username, pos);
+        if(ok){
+            for(Jogo j : lo.getJogosDecorrer()){
+                if(j.getCriador().equals(username) || j.getParticipante().equals(username)){
+                    j.adicionaJogada(new Jogada(username, pos, 0));
+                    break;
+                }
+            }
         }
-        
-        if(!ok){
+        else{
             RequestContext r = RequestContext.getCurrentInstance();
             r.execute("PF('dlg').show();");
         }
-        if(t==true){
-            return "/area_privada/gestaojogos?faces-redirect=true";
+        
+        /*if(t==true){
+            return "/area_privada/gestaojogos";
         }
         else{
-            return "/area_privada/jogo?faces-redirect=true";
+            return "/area_privada/jogo";
+        }*/
+    }
+    
+    public void atualiza(){  
+        List <Jogada> jog = null;
+        boolean criador = false;
+        Jogo jogo = null;
+        int [] jj = new int [10];
+        for(int i = 0; i < 10; i++){
+            jj[i] = -1;
         }
+        for(Jogo j : lo.getJogosDecorrer()){
+            if(j.getCriador().equals(username)){
+                jog = j.jogadas;
+                criador = true;
+                jogo = j;
+                break;
+            }
+            if(j.getParticipante().equals(username)){
+                jog = j.jogadas;
+                criador = false;
+                jogo = j;
+                break;
+            }
+        }
+        for(Jogada j : jog){
+            String id = "btn" + j.getPosX();
+            CommandButton btn = (CommandButton) findComponent(id);
+            btn.setDisabled(true);
+            if((j.getUserId().equals(username) && criador) || (!j.getUserId().equals(username) && !criador)){
+                btn.setValue("X");
+            }
+            else{
+                btn.setValue("O");
+            }
+            if(j.getUserId().equals(username)){
+                jj[j.getPosX()] = 1;
+            }
+            else{
+                jj[j.getPosX()] = 2;
+            }
+        }
+        if((jj[1] == 1 && jj[2] == 1 && jj[3] == 1)||(jj[4] == 1 && jj[5] == 1 && jj[6] == 1)||(jj[7] == 1 && jj[8] == 1 && jj[9] == 1)
+                ||(jj[1] == 1 && jj[4] == 1 && jj[7] == 1)||(jj[2] == 1 && jj[5] == 1 && jj[8] == 1)||(jj[3] == 1 && jj[6] == 1 && jj[9] == 1)
+                ||(jj[1] == 1 && jj[5] == 1 && jj[9] == 1) || (jj[3] == 1 && jj[5] == 1 && jj[7] == 1)){
+            jogo.setConcluido(true);
+            //jogo.setVencedor(username);
+            RequestContext r = RequestContext.getCurrentInstance();
+            r.execute("PF('dlgGanhar').show();");
+        }
+        if((jj[1] == 2 && jj[2] == 2 && jj[3] == 2)||(jj[4] == 2 && jj[5] == 2 && jj[6] == 2)||(jj[7] == 2 && jj[8] == 2 && jj[9] == 2)
+                ||(jj[1] == 2 && jj[4] == 2 && jj[7] == 2)||(jj[2] == 2 && jj[5] == 2 && jj[8] == 2)||(jj[3] == 2 && jj[6] == 2 && jj[9] == 2)
+                ||(jj[1] == 2 && jj[5] == 2 && jj[9] == 2) || (jj[3] == 2 && jj[5] == 2 && jj[7] == 2)){
+            jogo.setConcluido(true);
+            RequestContext r = RequestContext.getCurrentInstance();
+            r.execute("PF('dlgPerder').show();");
+        }
+        
+        if(jog.size() > 8){            
+            RequestContext r = RequestContext.getCurrentInstance();
+            r.execute("PF('dlgEmpate').show();");
+        }
+        
     }
 }
